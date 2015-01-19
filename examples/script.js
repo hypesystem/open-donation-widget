b70b50faf4a7875a764afc1bed0fc31aa9f222ca
Niels Abildgaard <niels.abildgaard@gmail.com>
2015-01-19 08:20:07 +0100

    Removed requirejs code from project

--- examples/script.js
+++ examples/script.js
@@ -1,17 +1,15 @@
-define(function(require) {
-    var moneyHandlers = [
-        new MoneyHandler("Payments Provider", 5, 0.25),
-        new MoneyHandler("Platform", 2, 0),
-        new MoneyHandler("Creator")
-    ];
-    var inputBox = document.querySelectorAll(".open-donation.donation-input")[0];
-    var input = new AmountField(inputBox, moneyHandlers);
-    var explainerBox = document.querySelectorAll(".open-donation.monetary-transparency-explainer")[0];
-    input.onChange(function(strings) {
-        explainerBox.innerHTML = "";
-        strings.forEach(function(str) {
-            explainerBox.innerHTML += "<div>" + str + "</div>";
-        });
+var moneyHandlers = [
+    new MoneyHandler("Payments Provider", 5, 0.25),
+    new MoneyHandler("Platform", 2, 0),
+    new MoneyHandler("Creator")
+];
+var inputBox = document.querySelectorAll(".open-donation.donation-input")[0];
+var input = new AmountField(inputBox, moneyHandlers);
+var explainerBox = document.querySelectorAll(".open-donation.monetary-transparency-explainer")[0];
+input.onChange(function(strings) {
+    explainerBox.innerHTML = "";
+    strings.forEach(function(str) {
+        explainerBox.innerHTML += "<div>" + str + "</div>";
     });
-    input.triggerChangeHandlers();
 });
+input.triggerChangeHandlers();
--- examples/setup-browser-require.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-    baseUrl: '../node_modules',
-    paths: {
-        script: '../examples/script',
-        local: '..'
-    }
-});
-
-requirejs.config(["script"], function() {});
